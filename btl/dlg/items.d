10f0bd0736cee72b81cb990ecc46621efcf4278f
GwendolyneFreddy <gwendolyne.freddy@gmail.com>
2021-10-19 14:37:39 +0200

    traification and Lollorian's small typo fixes
    - Source: http://www.shsforums.net/topic/45266-small-typo/

--- btl/dlg/items.d
+++ btl/dlg/items.d
@@ -3,108 +3,108 @@
 // Cromwell
 
 EXTEND_BOTTOM ~WSMITH01~ 58
-IF ~PartyHasItem("KovaDag2") XPGT("Kova", 999999)~ THEN REPLY ~Can you improve Navaeren?~ GOTO KvDag1
-IF ~PartyHasItem("KovaArm1") XPGT("Kova", 999999)~ THEN REPLY ~Is there a way to enhance Haeloven?~ GOTO KvArm1
-IF ~PartyHasItem("KiyoBow1") XPGT("Kiyone", 999999)~ THEN REPLY ~Can you improve Eye of the Sentinel?~ GOTO KyBow1
-IF ~PartyHasItem("KiyoArm1") XPGT("Kiyone", 999999)~ THEN REPLY ~Is there a way to enhance Verdant Vigil?~ GOTO KyArm1
+IF ~PartyHasItem("KovaDag2") XPGT("Kova", 999999)~ THEN REPLY @0 GOTO KvDag1
+IF ~PartyHasItem("KovaArm1") XPGT("Kova", 999999)~ THEN REPLY @1 GOTO KvArm1
+IF ~PartyHasItem("KiyoBow1") XPGT("Kiyone", 999999)~ THEN REPLY @2 GOTO KyBow1
+IF ~PartyHasItem("KiyoArm1") XPGT("Kiyone", 999999)~ THEN REPLY @3 GOTO KyArm1
 IF ~InParty("Yoshimo") PartyHasItem("Npsw02")
-Global("YoshiKnowCrom", "GLOBAL", 1)~ THEN REPLY ~Might you be able to alter Yoshimo's katana to be as versatile as Kova's weapon?~ GOTO YoshKata1
+Global("YoshiKnowCrom", "GLOBAL", 1)~ THEN REPLY @4 GOTO YoshKata1
 END
 
 APPEND ~WSMITH01~
 
 	IF ~~ THEN BEGIN YoshKata1
-	SAY ~Heh, that lad be bringing me good business, it seems. Let me see if ye be having the materials I need for it.~
+	SAY @5
 	IF ~PartyHasItem("Dagg15")~ THEN GOTO YoshKataHave
 	IF ~!PartyHasItem("Dagg15")~ THEN GOTO YoshKataNo
 	END
 
 	IF ~~ THEN BEGIN YoshKataNo
-	SAY ~Looks like ye haven't got the dagger I have to be needing for improving this katana of your friend's. Perhaps when you have found one you can ask me about it again.~
+	SAY @6
 	IF ~~ THEN GOTO 13
 	END
 
 	IF ~~ THEN BEGIN YoshKataHave
-	SAY ~Well it be seeming ye do have a dagger that's suitable after all. Now just the matter of the gold, it will cost ye 7,500 for such work with the forge.~
-	IF ~PartyGoldGT(7499)~ THEN REPLY ~That sounds fine. Let's do it.~ 
+	SAY @7
+	IF ~PartyGoldGT(7499)~ THEN REPLY @8
 	DO ~TakePartyGold(7500)      DestroyGold(7500)
 	TakePartyItemNum("Dagg15",1)   DestroyItem("Dagg15")
 	TakePartyItemNum("NPSW02",1)   DestroyItem("NPSW02")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
-	GiveItemCreate("YoshKata", "Yoshimo", 0,0,0)~ EXIT	
-	IF ~~ THEN REPLY ~Maybe another time.~ GOTO 13 
+	GiveItemCreate("YoshKata", "Yoshimo", 0,0,0)~ EXIT
+	IF ~~ THEN REPLY @9 GOTO 13
 	END
 
 	IF ~~ THEN BEGIN KvDag1
-	SAY ~Well ye be needing some scrolls, one with a Word to keep them foes still, another simply to snuff out a target's lifeforce. And a good amount of coin, 10,000 gold, no less, for the work.~
-	IF ~PartyHasItem("SCRL8Q") PartyHasItem("SCRL8O") PartyGoldGT(9999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~ 
+	SAY @10
+	IF ~PartyHasItem("SCRL8Q") PartyHasItem("SCRL8O") PartyGoldGT(9999)~ THEN REPLY @11
         DO ~TakePartyGold(10000)      DestroyGold(10000)
 	TakePartyItemNum("SCRL8Q",1)   DestroyItem("SCRL8Q")
 	TakePartyItemNum("SCRL8O",1)   DestroyItem("SCRL8O")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KvDagUpg", "GLOBAL", 1)
-	TakeItemReplace("KovaDag3","KovaDag2","Kova")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KovaDag3","KovaDag2","Kova")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 
 	IF ~~ THEN BEGIN KvArm1
-	SAY ~Well ye be needing a scroll to quicken actions without tiring, and an ordinary Ring of Protection. And not be forgetting the coin, 10,000 gold, no less, for the work.~
-	IF ~PartyHasItem("SCRL7Q") PartyHasItem("RING06") PartyGoldGT(9999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~ 
+	SAY @13
+	IF ~PartyHasItem("SCRL7Q") PartyHasItem("RING06") PartyGoldGT(9999)~ THEN REPLY @11
         DO ~TakePartyGold(10000)      DestroyGold(10000)
 	TakePartyItemNum("SCRL7Q",1)   DestroyItem("SCRL7Q")
 	TakePartyItemNum("RING06",1)   DestroyItem("RING06")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KvArmUpg", "GLOBAL", 1)
-	TakeItemReplace("KovaArm2","KovaArm1","Kova")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KovaArm2","KovaArm1","Kova")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 
 	IF ~~ THEN BEGIN KyBow1
-	SAY ~Well ye be needing to add twenty +2 arrows to some scrolls, three to dispel magic, and two to see things as they really are. And a good amount of coin, 10,000 gold, no less, for the work.~
-	IF ~NumItemsPartyGT("SCRL1E",2) 
+	SAY @14
+	IF ~NumItemsPartyGT("SCRL1E",2)
 	NumItemsPartyGT("SCRL7M",1)
 	NumItemsPartyGT("AROW11",19)
-	PartyGoldGT(9999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~
+	PartyGoldGT(9999)~ THEN REPLY @11
         DO ~TakePartyGold(10000)      DestroyGold(10000)
 	TakePartyItemNum("SCRL7M",2)   DestroyItem("SCRL7M")
 	TakePartyItemNum("SCRL1E",3)   DestroyItem("SCRL1E")
-	TakePartyItemNum("AROW11",20)   DestroyItem("ARROW11")
+	TakePartyItemNum("AROW11",20)   DestroyItem("AROW11")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KyBowUpg", "GLOBAL", 1)
-	TakeItemReplace("KiyoBow2","KiyoBow1","Kiyone")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KiyoBow2","KiyoBow1","Kiyone")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 
 	IF ~~ THEN BEGIN KyArm1
-	SAY ~I be needing to get a magical large shield effective at stopping arrows, called something about stars, a plain amulet of magic resistance, and some 10,000 gold for the work of putting it all together.~
+	SAY @15
 	IF ~PartyHasItem("SHLD07")
 	PartyHasItem("AMUL19")
-	PartyGoldGT(9999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~
+	PartyGoldGT(9999)~ THEN REPLY @11
         DO ~TakePartyGold(10000)      DestroyGold(10000)
 	TakePartyItemNum("SHLD07",1)   DestroyItem("SHLD07")
 	TakePartyItemNum("AMUL19",1)   DestroyItem("AMUL19")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KyArmUpg", "GLOBAL", 1)
-	TakeItemReplace("KiyoArm2","KiyoArm1","Kiyone")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KiyoArm2","KiyoArm1","Kiyone")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 END
 
-// Cespenar 
+// Cespenar
 
 EXTEND_BOTTOM ~BOTSMITH~ 2
-IF ~PartyHasItem("KovaDag3") XPGT("Kova", 3999999)~ THEN REPLY ~Can you further improve Navaeren?~ GOTO KvDag2
-IF ~PartyHasItem("KovaArm2") XPGT("Kova", 3999999)~ THEN REPLY ~Is there a way to enhance Haeloven even more?~ GOTO KvArm2
-IF ~PartyHasItem("KiyoBow2") XPGT("Kiyone", 3999999)~ THEN REPLY ~Can this bow get more powerful than just the Gaze of the Sentinel?~ GOTO KyBow2
-IF ~PartyHasItem("KiyoArm2") XPGT("Kiyone", 3999999)~ THEN REPLY ~Is there a way to make Verdant Vigilant even more potent?~ GOTO KyArm2
+IF ~PartyHasItem("KovaDag3") XPGT("Kova", 3999999)~ THEN REPLY @16 GOTO KvDag2
+IF ~PartyHasItem("KovaArm2") XPGT("Kova", 3999999)~ THEN REPLY @17 GOTO KvArm2
+IF ~PartyHasItem("KiyoBow2") XPGT("Kiyone", 3999999)~ THEN REPLY @18 GOTO KyBow2
+IF ~PartyHasItem("KiyoArm2") XPGT("Kiyone", 3999999)~ THEN REPLY @19 GOTO KyArm2
 END
 
 APPEND ~BOTSMITH~
 	IF ~~ THEN BEGIN KvDag2
-	SAY ~Hmm, oh need some ah, not so nice paper. Scroll to say Word then someone die, hmm, and more scroll, put thingy in air, when someone near then no can move. Two each, at least. And oh yeah, 15,000 gold.~
-	IF ~NumItemsPartyGT("SCRLAP",1) 
-	NumItemsPartyGT("SCRL9U",1) 
-	PartyGoldGT(14999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~
+	SAY @20
+	IF ~NumItemsPartyGT("SCRLAP",1)
+	NumItemsPartyGT("SCRL9U",1)
+	PartyGoldGT(14999)~ THEN REPLY @11
         DO ~TakePartyGold(15000)      DestroyGold(15000)
 	TakePartyItemNum("SCRL9U",1)   DestroyItem("SCRL9U")
 	TakePartyItemNum("SCRL9U",1)   DestroyItem("SCRL9U")
@@ -112,16 +112,16 @@ APPEND ~BOTSMITH~
 	TakePartyItemNum("SCRLAP",1)   DestroyItem("SCRLAP")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KvDagUpg", "GLOBAL", 2)
-	TakeItemReplace("KovaDag5","KovaDag3","Kova")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KovaDag5","KovaDag3","Kova")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 
 	IF ~~ THEN BEGIN KvArm2
-	SAY ~No problem, give 15,000 gold, and two scrolls each of Protection from Normal Weapons, Improved Haste. Oh and 4 potions, help make person invulnerable, colorful type. ~
-	IF ~NumItemsPartyGT("SCRL7Q",1) 
+	SAY @21
+	IF ~NumItemsPartyGT("SCRL7Q",1)
 	NumItemsPartyGT("SCRL6T",1)
 	NumItemsPartyGT("POTN11",3)
-	PartyGoldGT(14999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~
+	PartyGoldGT(14999)~ THEN REPLY @11
         DO ~TakePartyGold(15000)      DestroyGold(15000)
 	TakePartyItemNum("SCRL7Q",1)   DestroyItem("SCRL7Q")
 	TakePartyItemNum("SCRL6T",1)   DestroyItem("SCRL6T")
@@ -133,37 +133,37 @@ APPEND ~BOTSMITH~
 	TakePartyItemNum("POTN11",1)   DestroyItem("POTN11")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KvArmUpg", "GLOBAL", 2)
-	TakeItemReplace("KovaArm5","KovaArm2","Kova")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KovaArm5","KovaArm2","Kova")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 
 	IF ~~ THEN BEGIN KyBow2
-	SAY ~Let's see, about twenty +3 arrows and some spells, five to make magic go away, and three to make eyes good, oh yes. And some gold, not much, just 15,000 gold. Easy as pie.~
-	IF ~NumItemsPartyGT("SCRL1E",4) 
+	SAY @22
+	IF ~NumItemsPartyGT("SCRL1E",4)
 	NumItemsPartyGT("SCRL7M",2)
 	NumItemsPartyGT("AROW15",19)
-	PartyGoldGT(14999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~
+	PartyGoldGT(14999)~ THEN REPLY @11
         DO ~TakePartyGold(15000)      DestroyGold(15000)
 	TakePartyItemNum("SCRL7M",3)   DestroyItem("SCRL7M")
 	TakePartyItemNum("SCRL1E",5)   DestroyItem("SCRL1E")
-	TakePartyItemNum("AROW15",20)   DestroyItem("ARROW15")
+	TakePartyItemNum("AROW15",20)   DestroyItem("AROW15")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KyBowUpg", "GLOBAL", 2)
-	TakeItemReplace("KiyoBow3","KiyoBow2","Kiyone")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KiyoBow3","KiyoBow2","Kiyone")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 
 	IF ~~ THEN BEGIN KyArm2
-	SAY ~Ooh, this be very nice work when done, oh yes. Be needing very good cloak can protect, one scroll to protect from magic, and 15,000 gold to make it stay in one piece.~
+	SAY @23
 	IF ~PartyHasItem("CLCK02")
 	NumItemsParty("SCRL07",1)
-	PartyGoldGT(14999)~ THEN REPLY ~I've all the materials necessary and am willing to pay.~
+	PartyGoldGT(14999)~ THEN REPLY @11
         DO ~TakePartyGold(15000)      DestroyGold(15000)
 	TakePartyItemNum("CLCK02",1)   DestroyItem("CLCK02")
 	TakePartyItemNum("SCRL07",1)   DestroyItem("SCRL07")
 	CreateVisualEffectObject("SPCRTWPN",Myself)
 	SetGlobal("KyArmUpg", "GLOBAL", 2)
-	TakeItemReplace("KiyoArm3","KiyoArm2","Kiyone")~ EXIT	
-	IF ~~ THEN REPLY ~Maybe some other time.~ GOTO 13
+	TakeItemReplace("KiyoArm3","KiyoArm2","Kiyone")~ EXIT
+	IF ~~ THEN REPLY @12 GOTO 13
  	END
 END
